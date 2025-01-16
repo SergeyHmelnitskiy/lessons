8747cbb12ff2d41d641d486a13bd42848c91ff01
Sergey Hmelnitskiy <serg.hmelnitski@yandex.by>
2025-01-16 19:19:02 +0300

    add homework lesson6

---
+++ Maven/src/main/java/lesson6/MyArrayDataException.java
@@ -0,0 +1,8 @@
+package lesson6;
+
+public class MyArrayDataException extends RuntimeException{
+
+    public MyArrayDataException(String message) {
+        super(message);
+    }
+}
---
+++ Maven/src/main/java/lesson6/MyArraySizeException.java
@@ -0,0 +1,8 @@
+package lesson6;
+
+public class MyArraySizeException extends RuntimeException{
+
+    public MyArraySizeException(String massage) {
+        super(massage);
+    }
+}
